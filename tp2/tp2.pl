4623b5fffa25a3979de12892ec23ee8d1a502944
Sabrina Izcovich <sizcovich@dc.uba.ar>
2016-10-15 14:49:26 -0300

    ejercicio reduceLista

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -27,10 +27,14 @@ prefijos(L,P) :- append(P,_,L).
 %calculoDeTrazas(+Proceso, -Cadenas)
 calculoDetrazas(tau,[[]]).
 calculoDetrazas(Proc,L) :- reduceLista(Proc,M,0), prefijos(M,L).
-calculoDetrazas(P+Q,L):- calculoDetrazas(P,L1), calculoDetrazas(Q,L2), append([L1],[L2],L).
+calculoDetrazas(P+Q,L1):- calculoDetrazas(P,L1), calculoDetrazas(Q,_).
+calculoDetrazas(P+Q,L2):- calculoDetrazas(P,_), calculoDetrazas(Q,L2).
 
 %trazas(+Proceso, -Cadenas)
 trazas(Proc,M) :- setof(K,calculoDetrazas(Proc,K),M).
+
+%residuo(+X,+Cadena,-Qs)
+%residuo
 
 % Tests (van un par de ejemplos, agreguen los suyos).
 
