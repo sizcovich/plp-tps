c5b9d753c55090179ec551da1930b959d2ae6d53
Sonia Stedile <sileana@gmail.com>
2016-10-24 18:51:52 -0300

    agregue tests acciones

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -81,6 +81,13 @@ test(3) :- puedeReemplazarA(tau*a*0, a*0).
 test(4) :- equivalentes(a*b*(c*0+d*0), a*b*(d*tau*0+c*0)).
 test(5) :- not(equivalentes(a*b*0, b*a*0)).
 test(6) :- not(acciones(c*((a*0) + (b*tau*0)),[])).
-test(7) :-acciones((b*tau*0),[b]).
 
-tests :- forall(between(0, 7, N), test(N)). %Actualizar la cantidad total de tests para contemplar los que agreguen ustedes.
+%Tests de acciones
+test(7) :- acciones((b*tau*0),[b]).
+test(8) :- acciones((b*0),[b]).
+test(9) :- acciones(c*((a*0) + (b*tau*0)),[c,a,b]).
+
+%Tests de reduce
+
+
+tests :- forall(between(0, 9, N), test(N)). %Actualizar la cantidad total de tests para contemplar los que agreguen ustedes.
